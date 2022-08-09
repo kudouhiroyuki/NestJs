b3bc7eb62dccd6e4e0566797202bc0463fe84f68
‘kudou <h.kudou@ebacorp.jp>
2022-08-09 14:46:35 +0900

    Nest構築

--- src/institution/notice/notice.controller.ts
+++ src/institution/notice/notice.controller.ts
@@ -1,27 +1,31 @@
-import { Get, Post, Body, Controller, Render } from '@nestjs/common';
+import { Get, Post, Body, Controller, Render, Res, Redirect } from '@nestjs/common';
 
 @Controller('institution/notice')
 export class NoticeController {
-  
+  /**
+  * GET	お知らせ一覧画面
+  */
   @Get('/')
   @Render('institution/notice/index')
   async index() {
-    return {
-    };
+    return {};
   }
 
+  /**
+  * GET	お知らせ登録画面
+  */
   @Get('/create')
   @Render('institution/notice/create')
   async create() {
-    return {
-    };
+    return {};
   }
 
-  @Post('/')
-  @Render('institution/notice_inquiry')
-  send(@Body() body:any) {
-    console.log(body);
-  	return {
-    };
+  /**
+  * GET	お知らせ登録処理
+  */
+  @Post('/create')
+  @Redirect('http://localhost:3000/institution/notice')
+  store(@Body() body: any) {
+    console.log(body)
   }
 }
