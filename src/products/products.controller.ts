92397bd396a96064eab6eaf149c986a3f12236a1
‘kudou <h.kudou@ebacorp.jp>
2022-08-18 16:47:53 +0900

    商品一覧 ページネーション完了

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,6 +7,16 @@ export class ProductsController {
   */
   @Get('/reference')
   @Render('products/reference')
-  async index() {
+  async index(@Query() query: any) {
+    console.log("商品一覧画面:query")
+    console.log(query)
+    const products = [
+      { path: "/images/icon/products_item.svg", id: "1111", name: "商品A", date: "2022/08/01 - 2023/02/15" },
+      { path: "/images/icon/products_item.svg", id: "2222", name: "商品B", date: "2022/08/01 - 2023/02/15" },
+    ]
+    return {
+      products: JSON.stringify(products),
+      total_page_count: 3
+    };
   }
 }
