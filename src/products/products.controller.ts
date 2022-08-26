95ae634087d7e9d45d5e3ba3a24ff2d700fd855b
‘kudou <h.kudou@ebacorp.jp>
2022-08-26 12:45:53 +0900

    商品登録画面（セット）コーディング中

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -77,6 +77,7 @@ export class ProductsController {
       { id: '3', name: '1******03: ○○チケット' }
     ]
     const ages = [{ name: '大人' }, { name: '中人' }, { name: '小人' }, { name: '幼児' }, { name: 'シニア' }]
+    const age_relations = [{ name: '大人' }, { name: '高校生' }, { name: '中学生以下' }]
     const products = [
       {
         product_id: '1001',
@@ -87,17 +88,31 @@ export class ProductsController {
         stock_setting_management: 'dmp',
         stock_id: '1111',
         stock_name: '在庫A',
+        age_category: '中人',
         tenant_name: 'テナント名'
       },
       {
         product_id: '1001',
         product_name: 'B商品',
         plan_id: '2222',
-        plan_name: 'プランB',
+        plan_name: 'プランB-1',
         stock_setting_handover: true,
         stock_setting_management: 'dmp',
         stock_id: '2222',
-        stock_name: '在庫B',
+        stock_name: '在庫B-1',
+        age_category: '大人',
+        tenant_name: 'テナント名'
+      },
+      {
+        product_id: '1001',
+        product_name: 'B商品',
+        plan_id: '2222',
+        plan_name: 'プランB-2',
+        stock_setting_handover: true,
+        stock_setting_management: 'dmp',
+        stock_id: '2222',
+        stock_name: '在庫B-2',
+        age_category: '小人',
         tenant_name: 'テナント名'
       }
     ]
@@ -110,6 +125,7 @@ export class ProductsController {
     return {
       stock_dmps: JSON.stringify(stock_dmps),
       ages: JSON.stringify(ages),
+      age_relations: JSON.stringify(age_relations),
       products: JSON.stringify(products),
       tenants: JSON.stringify(tenants),
       tags: JSON.stringify(tags)
